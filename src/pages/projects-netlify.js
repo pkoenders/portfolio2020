eb33377fcad2ace62337bfd6f5bdb6c30cd3f6d5
Peter Koenders <pkoenders@gmail.com>
2020-08-02 04:28:58 +1200

    Update projects-netlify.js

--- src/pages/projects-netlify.js
+++ src/pages/projects-netlify.js
@@ -3,25 +3,23 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import SectionProjects from "../components/projects/projects-netlify-cms"
 
-const Projects
-  = () => {
-
-    return (
-      <>
-        <style type="text/css">
-          {`
+const Projects = ({ location }) => {
+  return (
+    <>
+      <style type="text/css">
+        {`
         body  {
           background-color: #efeff0;
         }
       `}
-        </style>
+      </style>
 
-        <Layout>
-          <SEO title="Projects" />
-          <SectionProjects />
-        </Layout >
-      </>
-    )
-  }
+      <Layout location={location}>
+        <SEO title="Projects" />
+        <SectionProjects />
+      </Layout >
+    </>
+  )
+}
 
 export default Projects
