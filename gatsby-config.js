256308ffd57478423b1e4869d812c9e4c2024eac
Peter Koenders <pkoenders@gmail.com>
2020-09-15 01:37:08 +1200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `Peter Koenders`,
     description: `Portfolio | Peter Koenders`,
     siteUrl: "https://pkoenders.netlify.app", // No trailing slash allowed!
-    defaultImage: "/static/images/svg/logo-pixl.inline.svg", // Path to your image you placed in the 'static' folder
+    defaultImage: "/images/svg/logo-pixl.inline.svg", // Path to your image you placed in the 'static' folder
     author: "Peter Koenders",
     year: "2020",
   },
