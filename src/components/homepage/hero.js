7f568f423bd47b3dcdd1797a3fc3a0ea44b772db
Peter Koenders <pkoenders@gmail.com>
2021-05-26 05:23:51 +1200

    aa

--- src/components/homepage/hero.js
+++ src/components/homepage/hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
     return (
         <section className={hero.wrapper + ' section-layout-wide contentStart'}>
             <div className={hero.intro}>
-                <a href="/peter-koenders-resume.pdf" className={"buttonPrimary"}><i className={"material-icons-round"} aria-hidden="true">article</i> View my resumé</a>
                 <h1><span>Tēnā koe,</span> welcome to my portfolio.</h1>
+                <a href="/peter-koenders-resume.pdf" className={"buttonPrimary"}><i className={"material-icons-round"} aria-hidden="true">article</i> View my resumé</a>
             </div>
         </section>
     )
