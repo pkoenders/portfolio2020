592d7645cced4c7a40e77d8bcd6230146c3e100d
Peter Koenders <pkoenders@gmail.com>
2021-05-27 04:13:41 +1200

    aa

--- src/components/common/footer/scrollToTop/index.js
+++ src/components/common/footer/scrollToTop/index.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react"
+import React from "react"
 import * as ScrollToTopStyles from './scrollToTop.module.scss'
 
 const ScrollToTop = () => {
-    const [showScroll, setShowScroll] = useState(false)
+    // const [showScroll, setShowScroll] = useState(false)
 
-    const checkScrollTop = () => {
-        if (!showScroll && window.pageYOffset > 400) {
-            setShowScroll(true)
-        } else if (showScroll && window.pageYOffset <= 400) {
-            setShowScroll(false)
-        }
-    };
+    // const checkScrollTop = () => {
+    //     if (!showScroll && window.pageYOffset > 400) {
+    //         setShowScroll(true)
+    //     } else if (showScroll && window.pageYOffset <= 400) {
+    //         setShowScroll(false)
+    //     }
+    // };
 
     const scrollTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' })
     }
-    if (typeof window !== 'undefined') {
-        window.addEventListener('scroll', checkScrollTop)
-    }
+    // if (typeof window !== 'undefined') {
+    //     window.addEventListener('scroll', checkScrollTop)
+    // }
 
     return (
         <button onClick={scrollTop} aria-label="Go to the top of the page" className={ScrollToTopStyles.btn} ><i className={" material-icons-round"} aria-hidden="true">arrow_upward</i></button>
